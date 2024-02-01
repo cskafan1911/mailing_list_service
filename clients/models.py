032800929a7922ee25fd6b9044f7bd9854029a68
Dmitry Dronov <cskafan1911@gmail.com>
2024-02-01 19:26:05 +0300

    Созданы приложения clients, log, mailing, main, users. Созданы модели Clients, Log, Mailing, Users, Message.

---
+++ clients/models.py
@@ -0,0 +1,31 @@
+from django.conf import settings
+from django.db import models
+
+from mailing.models import Mailing
+from users.models import NULLABLE
+
+
+class Clients(models.Model):
+    """
+    Класс модели клиентов
+    """
+
+    email = models.EmailField(verbose_name='почта')
+    first_name = models.CharField(max_length=100, verbose_name='Имя клиента', **NULLABLE)
+    last_name = models.CharField(max_length=100, verbose_name='Фамилия клиента', **NULLABLE)
+    comment = models.CharField(max_length=300, verbose_name='Комментарий клиента', **NULLABLE)
+    date_of_creation = models.DateTimeField(auto_now_add=True, verbose_name='Дата добавления', **NULLABLE)
+
+    mailing = models.ForeignKey('mailing.Mailing', on_delete=models.CASCADE,
+                                verbose_name='Рассылка')
+
+    def __str__(self):
+        """
+        Строковое представление клиента.
+        :return: Данные о клиенте
+        """
+        return f'Клиент пользователя: {self.mailing} - {self.first_name} {self.last_name} ({self.email})'
+
+    class Meta:
+        verbose_name = 'Клиент'
+        verbose_name_plural = 'Клиенты'
---
+++ mailing/models.py
@@ -0,0 +1,59 @@
+from django.conf import settings
+from django.db import models
+
+from users.models import NULLABLE
+
+
+class Mailing(models.Model):
+    """
+    Класс модели рассылки.
+    """
+
+    HOUR = 'HOUR'
+    WEEK = 'WEEK'
+    MONTH = 'MONTH'
+    FREQUENCY_CHOICES = [
+        (HOUR, 'раз в час'),
+        (WEEK, 'раз в неделю'),
+        (MONTH, 'раз в месяц'),
+    ]
+
+    COMPLETED = 'COMPLETED'
+    CREATED = 'CREATED'
+    LAUNCHED = 'LAUNCHED'
+    STATUS_CHOICES = [
+        (COMPLETED, 'завершена'),
+        (CREATED, 'создана'),
+        (LAUNCHED, 'запущена'),
+    ]
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='Рассылка пользователя')
+
+    name = models.CharField(max_length=50, verbose_name='Наименование рассылки', **NULLABLE)
+    time_start = models.DateTimeField(verbose_name='Время начала')
+    time_stop = models.DateTimeField(verbose_name='Время окончания')
+    frequency = models.CharField(max_length=5, choices=FREQUENCY_CHOICES, default=WEEK,
+                                 verbose_name='Периодичность рассылки')
+    status_mailing = models.CharField(max_length=10, choices=STATUS_CHOICES, default=COMPLETED,
+                                      verbose_name='Статус рассылки')
+
+    def __str__(self):
+        """
+        Строковое представление рассылки
+        :return: Имя пользователя, имя рассылки, статус рассылки, периодичность рассылки
+        """
+        return f'{self.user} - {self.name} ({self.status_mailing} - {self.frequency})'
+
+    class Meta:
+        verbose_name = 'Рассылка'
+        verbose_name_plural = 'Рассылки'
+
+
+class Message(models.Model):
+    """
+    Класс модели сообщения для рассылки.
+    """
+
+    subject = models.CharField(max_length=100, verbose_name='Тема письма')
+    message = models.TextField(verbose_name='Тело письма')
+    message_for_mailing = models.ForeignKey(Mailing, on_delete=models.CASCADE, verbose_name='Сообщение рассылки')
